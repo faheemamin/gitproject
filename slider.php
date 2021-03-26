c3e6cb6c4fb4c8aba11824b6e558d6d76d5267b1
Faheem <fahimamin017@gmail.com>
2021-03-26 17:12:28 +0500

    changes images

--- slider.php
+++ slider.php
@@ -56,17 +56,17 @@
         <script type="text/javascript">
         var dataHeader = [
                             {
-                              bigImage :"images/slide-1.jpg",
+                              bigImage :"images/one.jpg",
                               title : "Minimal & Clean",
 							  author : "Templatestock"
                             },
                             {
-                              bigImage :"images/slide-2.jpg",
+                              bigImage :"images/020.jpg",
                               title : "I'm A Creative Theme",
                               author : "Templatestock"
                             },
                             {
-                              bigImage :"images/slide-3.jpg",
+                              bigImage :"images/030.jpg",
                               title : "Achieve Success",
                               author : "Templatestock"
                             }
