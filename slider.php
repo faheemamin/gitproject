71137942c5880b73ac366207feb9915565595c86
Faheem <fahimamin017@gmail.com>
2021-03-30 13:13:13 +0500

    change pics

--- slider.php
+++ slider.php
@@ -56,17 +56,17 @@
         <script type="text/javascript">
         var dataHeader = [
                             {
-                              bigImage :"images/slide-1.jpg",
+                              bigImage :"images/1.jpg",
                               title : "Minimal & Clean",
 							  author : "Templatestock"
                             },
                             {
-                              bigImage :"images/slide-2.jpg",
+                              bigImage :"images/2.jpg",
                               title : "I'm A Creative Theme",
                               author : "Templatestock"
                             },
                             {
-                              bigImage :"images/slide-3.jpg",
+                              bigImage :"images/3.jpg",
                               title : "Achieve Success",
                               author : "Templatestock"
                             }
